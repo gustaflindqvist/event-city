9c72be2f928da0edcad8d69be8c5c4860343f1c3
Gustaf Lindqvist <gustaf@savecore.se>
2013-02-05 00:22:22 +0100

    Testing Nokogiri for ruby to parse html

--- lib/parsing.rb
+++ lib/parsing.rb
@@ -1,10 +1,19 @@
 require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
 
-module events
+module Events
   class Parsing
+    def initialize(url)
+      @doc = Nokogiri::HTML(open(url))
+    end
 
-    def initialize
-      # Stub
+    def parse_from_url
+      @doc.xpath('//span[@class="method-name"]').each do | method_span |
+          puts method_span.content
+          puts method_span.path
+          puts
+      end
     end
   end
 end
