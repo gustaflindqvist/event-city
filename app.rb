13d5724feefdd57a604db984e198db970aa4b200
Gustaf Lindqvist <gustaf@savecore.se>
2013-02-09 17:03:17 +0100

    Added support for static files. Extract the client code to eventcity.js.

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 
-get '/' do
-  erb :index
+class EventCity < Sinatra::Base
+  set :static, true
+  set :root, File.dirname(__FILE__)
+  set :public_folder, Proc.new { File.join(root, "static") }
+
+  get '/' do
+    erb :index
+  end
 end
---
+++ static/js/eventcity.js
@@ -0,0 +1,66 @@
+var EventCity = {
+    map: null,
+    marker: null,
+
+    getPosition: function() {
+        if(!navigator.geoLocation) {
+            EventCity.noGeoSupport();
+            return;
+        }
+
+        var loader = $(".map-container .loader");
+        loader.show();
+        $(".map-container .fail").hide();
+
+        var watchPositionId = navigator.geolocation.watchPosition(function(position) {
+            console.log("position:", position);
+            var reverseUrl = "/lookup/" + position.coords.latitude + "/" + position.coords.longitude;
+            $.get(reverseUrl, function(response) {
+                console.log("response:", response);
+            }).fail(function() {
+                EventCity.failMessage("Fail")
+            });
+        }, function() {
+            EventCity.failMessage("You have to allow the website to know your location");
+        });
+    },
+
+    onLocationFound: function() {
+        var radius = e.accuracy / 2;
+
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+    },
+
+    initMap: function() {
+        $("map").show();
+
+        if (!this.map) {
+            var map = L.map('map', {zoomControl:false});
+            L.tileLayer('http://{s}.tile.cloudmade.com/33f1c74149b04476931958e293559044/997/256/{z}/{x}/{y}.png', {attribution: ''}).addTo(map);
+            map.attributionControl.setPrefix('');
+            map.locate({setView: true, maxZoom: 16});
+            this.map = map;
+        }
+        return this.map;
+    },
+
+    addMarker: function(point) {
+        if (this.marker)
+            this.map.removeLayer(this.marker);
+        this.marker = L.marker(point).addTo(this.map);
+    },
+
+    noGeoSupport: function() {
+        console.log("Your browser doesn't support geo coding.");
+    },
+
+    failMessage: function(message) {
+        $(".map-container .loader").hide();
+        $(".map-container .fail").show();
+        $("#map").hide();
+        $(".position-info").html("<p>" + message + "</p>");
+    }
+}
